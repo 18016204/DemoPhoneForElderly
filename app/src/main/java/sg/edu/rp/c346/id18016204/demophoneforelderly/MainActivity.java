91d84a50ed597c2ed709124e11c5ffc391cb7c5f
18016204 <57203937+18016204@users.noreply.github.com>
2020-06-02 13:18:01 +0800

    done

--- app/src/main/java/sg/edu/rp/c346/id18016204/demophoneforelderly/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id18016204/demophoneforelderly/MainActivity.java
@@ -2,13 +2,41 @@ package sg.edu.rp.c346.id18016204.demophoneforelderly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
+
+Button son ;
+Button notSon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        son = findViewById(R.id.buttonCallSon);
+        notSon = findViewById(R.id.buttonCallDaughter);
+        son.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 92240336));
+                startActivity(intentCall);
+            }
+        });
+        notSon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 89675543));
+                startActivity(intentCall);
+            }
+        });
     }
+
+
 }
